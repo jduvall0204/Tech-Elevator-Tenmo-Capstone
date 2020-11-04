8a56cbf67e1a6479b56ade6fdb33f0a0efb4f9af
August Babington <augustbabington@gmail.com>
2020-11-04 17:10:11 -0500

    AccountController, UserSqlDAO adds

--- TenmoServer/DAO/IUserDAO.cs
+++ TenmoServer/DAO/IUserDAO.cs
@@ -8,5 +8,7 @@ namespace TenmoServer.DAO
         User GetUser(string username);
         User AddUser(string username, string password);
         List<User> GetUsers();
+        User GetBalance(double amount);
+        User Update(int id, User user);
     }
 }
